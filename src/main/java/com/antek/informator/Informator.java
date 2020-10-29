5e1ba1ca9bcd91e657fe0e772ad6a9395c60d8ae
Antoni <GrabowskiAntoni@icloud.com>
2020-10-29 22:10:39 +0100

    1

--- src/main/java/com/antek/informator/Informator.java
+++ src/main/java/com/antek/informator/Informator.java
@@ -1,45 +1,51 @@
 package com.antek.informator;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Informator {
-    public static void main(String... args) {
-        Osoba antek = new Osoba("12345678910");
-        Osoba hania = new Osoba("12345678345");
-        Osoba anetta = new Osoba("82456213787");
-        Osoba marcin = new Osoba("13467324958");
-        Osoba toska = new Osoba("29576392513");
+    public static void main(String... args) throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        String line = buffer.readLine();
+        if (line != "exit") {
+
+            Osoba antek = new Osoba("12345678910");
+            Osoba hania = new Osoba("12345678345");
+            Osoba anetta = new Osoba("82456213787");
+            Osoba marcin = new Osoba("13467324958");
+            Osoba toska = new Osoba("29576392513");
 
 
-        antek.setImie("Antoni");
-        antek.setNazwisko("Grabowski");
-        antek.setMama(anetta);
-        antek.setTata(marcin);
+            antek.setImie("Antoni");
+            antek.setNazwisko("Grabowski");
+            antek.setMama(anetta);
+            antek.setTata(marcin);
 
-        hania.setImie("Hanna");
-        hania.setNazwisko("Grabowska");
-        hania.setMama(anetta);
-        hania.setTata(marcin);
+            hania.setImie("Hanna");
+            hania.setNazwisko("Grabowska");
+            hania.setMama(anetta);
+            hania.setTata(marcin);
 
-        anetta.setImie("Anetta");
-        anetta.setNazwisko("Grabowska");
-        anetta.setDzieci(new ArrayList<>());
-        anetta.getDzieci().add(antek);
-        anetta.getDzieci().add(hania);
-        anetta.getDzieci().add(toska);
+            anetta.setImie("Anetta");
+            anetta.setNazwisko("Grabowska");
+            anetta.setDzieci(new ArrayList<>());
+            anetta.getDzieci().add(antek);
+            anetta.getDzieci().add(hania);
+            anetta.getDzieci().add(toska);
 
 
-        marcin.setImie("Marcin");
-        marcin.setNazwisko("Grabowski");
-        marcin.setDzieci(new ArrayList<>());
-        marcin.getDzieci().add(antek);
-        marcin.getDzieci().add(hania);
+            marcin.setImie("Marcin");
+            marcin.setNazwisko("Grabowski");
+            marcin.setDzieci(new ArrayList<>());
+            marcin.getDzieci().add(antek);
+            marcin.getDzieci().add(hania);
 
-        System.out.println(antek);
-        System.out.println(hania);
-        System.out.println(anetta);
-        System.out.println(marcin);
+            System.out.println(antek);
+            System.out.println(hania);
+            System.out.println(anetta);
+            System.out.println(marcin);
+
+        }
 
     }
-
 }
