e83880f74831950983e9b93e49638632711c17d5
Antoni <GrabowskiAntoni@icloud.com>
2020-11-03 23:04:40 +0100

    Added new features

--- src/main/java/com/antek/informator/Informator.java
+++ src/main/java/com/antek/informator/Informator.java
@@ -2,60 +2,32 @@ package com.antek.informator;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Informator {
+    public static HashMap<String, Osoba> personByPesel = new HashMap<>();
     public static void main(String... args) throws IOException {
         System.out.println("Witaj\nWybierz opcje:\n1 - Wyświetl osoby\n2 - Rejestruj osobe\nAby zakonczyc program wpisz exit");
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        String line = buffer.readLine();
-        while(!line.equals("exit")){
-            String opcja = buffer.readLine();
-            if(opcja.equals("1")) {
-                System.out.println("Tutaj wyświetlimy osoby");
+        while(true){
+            String line = buffer.readLine();
+            switch(line){
+                case "1":
+                    //printPerson(""); // metoda przyjmujaca string pesel wyswietla osobe po peselu
+                    break;
+                case "2":
+                    //registerPerson(); // metoda rejestruj osobe
+                    break;
+                case "exit":
+                    System.exit(0);
             }
-            if(opcja.equals("2")) {
-                System.out.println("Tutaj rejestrujemy osobe");
-            }
-            if(opcja.equals("exit")) break;
         }
-        System.exit(0);
-//            Osoba antek = new Osoba("12345678910");
-//            Osoba hania = new Osoba("12345678345");
-//            Osoba anetta = new Osoba("82456213787");
-//            Osoba marcin = new Osoba("13467324958");
-//            Osoba toska = new Osoba("29576392513");
-//
-//
-//            antek.setImie("Antoni");
-//            antek.setNazwisko("Grabowski");
-//            antek.setMama(anetta);
-//            antek.setTata(marcin);
-//
-//            hania.setImie("Hanna");
-//            hania.setNazwisko("Grabowska");
-//            hania.setMama(anetta);
-//            hania.setTata(marcin);
-//
-//            anetta.setImie("Anetta");
-//            anetta.setNazwisko("Grabowska");
-//            anetta.setDzieci(new ArrayList<>());
-//            anetta.getDzieci().add(antek);
-//            anetta.getDzieci().add(hania);
-//            anetta.getDzieci().add(toska);
-//
-//
-//            marcin.setImie("Marcin");
-//            marcin.setNazwisko("Grabowski");
-//            marcin.setDzieci(new ArrayList<>());
-//            marcin.getDzieci().add(antek);
-//            marcin.getDzieci().add(hania);
-//
-//            System.out.println(antek);
-//            System.out.println(hania);
-//            System.out.println(anetta);
-//            System.out.println(marcin);
+    }
 
-
+    private static String printPerson(String pesel){
+        return "pesel";
+    }
+    private static void registerPerson(Osoba rejestrowana){
 
     }
 }
